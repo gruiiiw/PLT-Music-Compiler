594aea98cdabe52c595ef24334c0689afac1f039
Ben Cyna <benjamincyna@gmail.com>
2024-10-10 14:13:46 -0400

    to do list made in scripting file

---
+++ lexicalAnalyser.py
@@ -0,0 +1,20 @@
+# TODO define all Identifiers, Operators, Keywords, Operators, Delimiters, And whitespace and add it to the readme, also have it in table
+
+
+# TODO scanning algorithm 
+
+# TODO a. Scan the input programs written in your language and output a list of tokens in the following format: <Token Type, Token Value>.
+def scan():
+    pass
+
+# TODO Show state transitions DFA (put in in a matrix)
+
+# TODO handle lexical errors 
+def handleErrors():
+    pass
+
+# TODO Provide 5 sample input programs
+
+# TODO Make an execution script that will run the lexical analyzer or provide instructions on how to do soc
+
+
