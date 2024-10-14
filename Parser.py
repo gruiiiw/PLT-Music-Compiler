0e3aa0d7ad15f6a39270351d48a10904538331b1
Ben Cyna <benjamincyna@gmail.com>
2024-10-14 10:24:46 -0400

    implimented scanner

--- Parser.py
+++ Parser.py
@@ -2,34 +2,37 @@ class Parser:
     def __init__(self) -> None:
         self.current_state = 0 #start]
         self.stateMachine = self.buildStateMachine()
+        self.token_states_arrive = {2: "Note", 6: "Operator", 27: "Keyword", 14: "Note", 15: "Delimitter", 20: "Whitespace"}
+        self.token_states_leave = {4: "Identifier", 2: "Whitespace", 10: "Delimitter", 16: "Identifier", 18: "Integer", 24: "Keyword"}
+        self.final_states = {0, 2,15}
      
     # the main issue with our state machine is that there are a couple of states that require more specific inputs i.e. state 10 requires us to check for p then l then a then y
     # so we have to add that functionality but that might blow up the state machine a bit.     
     def buildStateMachine(self):
         state_machine = [
-            # A-G 0,  1-8 1, whqes 2, whitespace\n 3, delimiter 4, operator 5, identifier 6  [H-Z] 7 , keyword 8,   id[a-z] 9   ( 10   0 11   [0-9] 12    p 13, l 14, a 15, y16, t17, i18, m19, e20, s21   
+            # A-G 0,  1-8 1, whqes 2, whitespace\n 3, delimiter 4, operator 5, identifier 6  [H-Z] 7 , keyword 8,   id[a-z] 9   ( 10   0 11   [0-9] 12    p 13, l 14, a 15, y16, t17, i18, m19, e20, s21   # unused col 6 and 8 row 7 and 9
              
-            [8,     18,        33,     0,                 33,         33,          33,           5,        33,          33,       33,     33,      18,      33,    33,  33,   33,  33,   33,  33, 33, 33],#start do nothing Accept state 
+            [8,     18,        33,     0,                 33,         33,          33,           5,        33,          33,       33,     33,      18,      22,    33,  33,   33,  33,   33,  33, 33, 33],#start do nothing Accept state 
             [33,     33,        2,     33,                33,         33,          33,           33,        33,          33,       33,     33,      33,      33,    33,  33,   33,  33,   33,  33, 33, 33],#q1 
             [3,     33,        33,     0,                 33,         33,          33,           33,        33,          33,       33,     33,      33,      33,    33,  33,   33,  33,   33,  33, 33, 33],#q2 NOTE accept state (program can terminate if here)
             [33,     1,        33,     33,                33,         33,          33,           33,        33,          33,       33,     33,      33,      33,    33,  33,   33,  33,   33,  33, 33, 33],#q3 start of note
-            [33,     33,        33,     33,                33,         6,          33,           33,        33,          4,       33,     33,      33,      33,    33,  33,   33,  33,   33,  33, 33, 33],#q4 identifier
-            [33,     33,        33,     33,                33,         33,          33,           33,        33,          4,       33,     33,      33,      33,    33,  33,   33,  33,   33,  33, 33, 33],#q5
+            [33,     33,        33,     33,                33,         33,          33,           33,        33,          4,       33,     33,      33,      4,    4,  4,   4,  4,   4,  4, 4, 4],#q4 identifier
+            [33,     33,        33,     33,                33,         33,          33,           33,        33,          4,       33,     33,      33,      4,    4,  4,   4,  4,   4,  4, 4, 4],#q5
             [3,      33,        33,     33,                33,         33,          33,           33,        33,          33,       33,     33,      33,      33,    33,  33,   33,  33,   33,  33, 33, 33],#q6 operator 
             [33,     33,        33,     33,                33,         33,          33,           33,        33,          33,       33,     33,      33,      33,    33,  33,   33,  33,   33,  33, 33, 33],#q7 i didnt include q7 on my state machine my b
-            [33,     1,        33,     33,                33,         33,          33,           33,        33,          4,       33,     33,      33,      33,    33,  33,   33,  33,   33,  33, 33, 33],#q8
+            [33,     1,        33,     33,                33,         33,          33,           33,        33,          4,       33,     33,      33,      4,    4,  4,   4,  4,   4,  4, 4, 4],#q8
             [33,     33,        33,     33,                33,         33,          33,           33,        33,          33,       33,     33,      33,      33,    33,  33,   33,  33,   33,  33, 33, 33],#q9 no q9 either
             [33,     33,        33,     33,                33,         33,          33,           33,        33,          33,       33,     33,      33,      33,    22,  33,   33,  33,   33,  33, 33, 33],#q10 KEYWORD if we get p->l->a->y we go stay here, then when we get ( we go to q11 not sure how to code this
             [12,     33,        33,     33,                33,         33,          33,           17,        33,          33,       33,     33,      33,      33,    33,  33,   33,  33,   33,  33, 33, 33],#q11 Dilimiter (
-            [33,     13,        33,     33,                33,         33,          33,           33,        33,          16,       33,     33,      33,      33,    33,  33,   33,  33,   33,  33, 33, 33],#q12
+            [33,     13,        33,     33,                33,         33,          33,           33,        33,          16,       33,     33,      33,      16,    16,  16,   16,  16,   16,  16, 16, 16],#q12
             [33,     33,        14,     33,                33,         33,          33,           33,        33,          33,       33,     33,      33,      33,    33,  33,   33,  33,   33,  33, 33, 33],#q13
             [33,     33,        33,     33,                33,         33,          33,           33,        33,          33,       15,     33,      33,      33,    33,  33,   33,  33,   33,  33, 33, 33],#q14 NOTE close bracket go 15
-            [8,     18,        33,     0,                 33,         33,          33,           5,        33,          33,       33,     33,      18,      33,    33,  33,   33,  33,   33,  33, 33, 33],#q15 Delimitter and go to start (Accept state)
+            [8,     18,        33,     0,                 33,         33,          33,           5,        33,          33,       33,     33,      18,      22,    33,  33,   33,  33,   33,  33, 33, 33],#q15 Delimitter and go to start (Accept state)
             [33,     33,        33,     33,                33,         33,          33,           33,        33,          33,       33,     15,      33,      33,    33,  33,   33,  33,   33,  33, 33, 33],#q16 IDENTIFIER, stays here on a-z, moves to 15 on ) and sets to ID
-            [33,     33,        33,     33,                33,         33,          33,           33,        33,          16,       33,     33,      33,      33,    33,  33,   33,  33,   33,  33, 33, 33],#q17 in an identifier 
+            [33,     33,        33,     33,                33,         33,          33,           33,        33,          16,       33,     33,      33,      16,    16,  16,   16,  16,   16,  16, 16, 16],#q17 in an identifier 
             [33,     18,        33,     33,                33,         33,          33,           33,        33,          33,       33,     33,      18,      33,    33,  33,   33,  19,   33,  33, 33, 33],#q18 INT, if we get a t go to q25
             [33,     33,        33,     33,                33,         33,          33,           33,        33,          33,       33,     33,      33,      33,    33,  33,   33,  33,   25,  33, 33, 33],#q19 Whitespace
-            [88,     18,        33,     0,                 33,         33,          33,           5,        33,          33,       33,     33,      18,      33,    33,  33,   33,  33,   33,  33, 33, 33],#q20 go back to start state (not accept) 
+            [88,     18,        33,     0,                 33,         33,          33,           5,        33,          33,       33,     33,      18,      22,    33,  33,   33,  33,   33,  33, 33, 33],#q20 go back to start state (not accept) 
             [33,     33,        33,     33,                33,         33,          33,           33,        33,          33,       33,     33,      33,      33,    33,  23,   33,  33,   33,  33, 33, 33],#q22l->a
             [33,     33,        33,     33,                33,         33,          33,           33,        33,          33,       33,     33,      33,      33,    33,  33,   24,  33,   33,  33, 33, 33],#q23a->y
             [33,     33,        33,     33,                33,         33,          33,           33,        33,          33,       33,     33,      33,      33,    33,  33,   33,  11,   33,  33, 33, 33],#q24y->11
@@ -45,7 +48,111 @@ class Parser:
         ]
         
         
+    # TODO build state machine matrix
+    def getStateCode(value):
+          # p 13, 
+        if value == "p":
+            return 13
         
-    def get_next_state(self):
-        pass
+        if value == "l":
+            return 14
+        if value == "a":
+            return 15
+        if value == "y":
+            return 16
+        if value == "t":
+            return 17
+        if value == "i":
+            return 18
+        if value == "m":
+            return 19
+        if value == "e":
+            return 20
+        if value == "s":
+            return 21
+        
+        
+        if value in "ABCDEFG":
+            return 0
+        
+        if value.isdigit() and int(value) in range(1, 9):
+            return 1
+        
+        #whqes 2
+        if value in "whqes":
+            return 2
+        
+        # , whitespace\n 
+        if value in "\n\t ":
+            return 3
+        
+        # 4, delimiter 
+        if value in ":(){}":
+            return 4
+        
+        # 5, operator 
+        if value == "=":
+            return 5
+        
+        # identifier 6 skip 
+        
+        # [H-Z] 7 
+        if value in "HIJKLMNOPQRSTUVWXYZ":
+            return 7
+        
+        # , keyword 8,  skip 
+        
+        # id[a-z] 9   
+        if value in "abcdefghijklmnopqrstuvwxyz":
+            return 9
+        
+        
+        # ( 10   
+        if value == '(':
+            return 10
+        
+        
+        # 0 11   
+        if value == "(":
+            return 11 
+        
+        
+        # [0-9] 12    
+        if value.isdigit():
+            return 12
+        
+        return 33
+        
+    def scan(self, string):
+        lexical_analysis = []
+        cur_seq = []
+        cur_state = 0
+        for char in string:
+            if char == ' ':
+                continue
+            
+            if cur_state in self.token_states_arrive:
+                lexical_analysis.append(self.token_states_arrive[cur_state], "".join(cur_seq))
+                cur_seq = []
+            
+            cur_seq.append(char)
+            
+            cur_state = self.stateMachine[cur_state][self.getStateCode(char)]
+            
+            if cur_state == 33:
+                return self.handleErrors(f"Invalid character {char}")
+            
+            if cur_state in self.token_states_leave:
+                lexical_analysis.append(self.token_states_leave[cur_state], "".join(cur_seq))
+                cur_seq = []
+        
+        if cur_state in self.final_states:
+            return lexical_analysis
+        
+        return self.handleErrors("Not in a ending final state")
+            
+                
+                
     
+    def handleErrors(self, err):
+        return [err]
--- lexicalAnalyser.py
+++ lexicalAnalyser.py
@@ -1,53 +1,11 @@
-import re
+from Parser import Parser
+data = "times A4 = 4 play A4"
 
-# define all Identifiers, Operators, Keywords, Delimiters, And whitespace and add it to the readme, also have it in table
-KEYWORD = ["times", "play"]
-NUMBERS = [0-9]
-IDENTIFIER = ['A-Z']['a-z']*
-OPERATORS = ["="]
-NOTE = [A-G][1-8][w|h|q|e|s]
-WHITESPACE = ['\n', '\t', ' ']
-DELIMITER = [":","(", ")", '{', '}']
+parser = Parser()
+lexical_analysis = parser.scan(data)
 
-# TODO build state machine matrix
-def getStateCode(value):
-    if re.match(r'[A-G]', value): # we're not allowed to use the regular expression library unfortunately
-        return 0
+print(lexical_analysis)   
     
-    if re.match(r'[1-8]', value):
-        return 1
-    
-    if re.match(r'[whqes]', value):
-        return 2
-    
-    if re.match(r'[\n]', value):
-        return 3
-    
-    if re.match(r'[():{}]', value):
-        return 4
-    
-    if re.match(r'[=]', value):
-        return 5
-    
-    if re.match(r'[A-Za-z]', value):
-        return 6
-    
-    if re.match(r'[tp]', value):
-        return 7
-    
-    if re.match(r'[H-Z]', value):
-        return 8
-    
-
-# TODO executre scanning algorithm to produce error or lexical tokens
-
-# TODO a. Scan the input programs written in your language and output a list of tokens in the following format: <Token Type, Token Value>.
-def scan():
-    pass
-
-# TODO handle lexical errors 
-def handleErrors():
-    pass
 
 # TODO Provide 5 sample input programs
 
